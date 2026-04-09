adb93f2fd19f5ebfe5f68bc02643a573c09ddfb8
Guilherme Rodrigues <guilhermerodrigues6484@gmail.com>
2026-04-09 01:25:30 -0300

    corrindo o infocard

--- src/features/hero/infoCard/components/Headline.tsx
+++ src/features/hero/infoCard/components/Headline.tsx
@@ -9,10 +9,10 @@ export function Headline() {
       </p>
       <p className="text-base text-zinc-900 leading-relaxed mb-6">
         Desenvolvedor apaixonado por criar produtos digitais de alto impacto.
-        Especialista em soluções <strong>SaaS</strong>.{' '}
+        Especialista em soluções <strong>SaaS</strong> e <strong>Landing Pages</strong>.{' '}
         Formando em Análise e Desenvolvimento de Sistemas pelo{' '}
         <strong>IFNMG</strong> e atuando também como{' '}
-        <strong>Freelancer</strong> em projetos web e mobile.
+        <strong>Freelancer</strong> em projetos web.
       </p>
     </>
   )
